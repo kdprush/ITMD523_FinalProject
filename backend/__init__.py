925ce715d94dfe28924ec048bfceb9d0fb132982
Kat Prush <kprush@hawk.iit.edu>
2024-12-02 22:29:53 -0600

    updated @ with %40

--- backend/__init__.py
+++ backend/__init__.py
@@ -7,7 +7,7 @@ def create_app():
     app = Flask(__name__)
 
     # Directly set Flask configurations with no placeholders
-    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:MySQL@1234@localhost:3306/freelancer_marketplace"
+    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:MySQL%401234@localhost:3306/freelancer_marketplace"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SECRET_KEY'] = "your_secret_key"
 
